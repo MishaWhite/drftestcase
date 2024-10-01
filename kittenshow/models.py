182abe42f0166f09db081b7e5c061419f3029fa8
Misha_White <memantulov@mail.ru>
2024-10-02 00:43:16 +0700

    Add Models

--- kittenshow/models.py
+++ kittenshow/models.py
@@ -1,4 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+class KittenBreed(models.Model):
+    name = models.CharField('Название породы', max_length=150, unique=True)
+
+
+class Color(models.Model):
+    name = models.CharField('Наименование цвета/окраса', max_length=150, unique=True)
+
+
+class Kitten(models.Model):
+    name = models.CharField('Кличка котёнка', max_length=150)
+    color = models.ForeignKey(Color, verbose_name='Цвет/окрас', on_delete=models.PROTECT)
+    аge = models.IntegerField('Возраст, мес.')
+    description = models.TextField('Описание')
+    user_add = models.ForeignKey(User, on_delete=models.PROTECT)
